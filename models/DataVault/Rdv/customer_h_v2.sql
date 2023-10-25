5db5c0c477e2cdfcd35b8aa20c0ebc56735bb6ab
Jaroslaw Syrokosz <jsyrokosz@datauniversum.com>
2023-10-25 14:30:29 +0000

    customer hub with alias

---
+++ models/DataVault/Rdv/customer_h_v2.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='incremental') }}
+
+{%- set yaml_metadata -%}
+hashkey: 'hk_customer_h'
+business_keys: 
+    - CUST_NAME
+source_models: 
+    - name: stg_customer
+      bk_columns:
+        - C_NAME as CUST_NAME
+      rsrc: '*Customers*'
+{%- endset -%}
+
+{%- set metadata_dict = fromyaml(yaml_metadata) -%}
+
+{%- set hashkey = metadata_dict['hashkey'] -%}
+{%- set business_keys = metadata_dict['business_keys'] -%}
+{%- set source_models = metadata_dict['source_models'] -%}
+
+{{ datavault4dbt.hub(hashkey=hashkey,
+                    business_keys=business_keys,
+                    source_models=source_models) }}
