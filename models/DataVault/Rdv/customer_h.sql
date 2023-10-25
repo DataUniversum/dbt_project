83585360a0a173b5ac7add1da45ba963429d298b
Jaroslaw Syrokosz <jsyrokosz@datauniversum.com>
2023-10-25 13:02:55 +0000

    Customer data in RDV

---
+++ models/DataVault/Rdv/customer_h.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='incremental') }}
+
+{%- set yaml_metadata -%}
+hashkey: 'hk_customer_h'
+business_keys: 
+    - C_NAME
+source_models: stg_customer
+{%- endset -%}
+
+{%- set metadata_dict = fromyaml(yaml_metadata) -%}
+
+{%- set hashkey = metadata_dict['hashkey'] -%}
+{%- set business_keys = metadata_dict['business_keys'] -%}
+{%- set source_models = metadata_dict['source_models'] -%}
+
+{{ datavault4dbt.hub(hashkey=hashkey,
+                    business_keys=business_keys,
+                    source_models=source_models) }}
--- models/DataVault/Stages/stg_customer.sql
+++ models/DataVault/Stages/stg_customer.sql
@@ -1,35 +1,33 @@
 
-{{ config(materialized='view') }}
+{{config(materialized='view')}}
 
 {%- set yaml_metadata -%}
-    source_model:
-        'tpch':'customer'
-    ldts: 
-    rsrc: '!tpch1'
-    hashed_columns:
-        hk_customer_h:
-            - C_NAME
-        hd_customer_s:
-            is_hashdiff: true
-            columns:
-                - C_ADDRESS
-                - C_NATIONKEY
-                - C_PHONE
-                - C_MKTSEGMENT
+source_model:
+    'tpch': 'customer'
+hashed_columns:
+    hk_customer_h:
+        - C_NAME
+    hd_customer_s:
+        is_hashdiff: true
+        columns:
+            - C_ADDRESS
+            - C_NATIONKEY
+            - C_PHONE
+            - C_MKTSEGMENT
+ldts: 'current_timestamp'
+rsrc: '!tpch1'
 {%- endset -%}
-
-    {%- set metadata_dict = fromyaml(yaml_metadata) -%}
-    {%- set source_model = metadata_dict['source_model'] -%}
-    {%- set ldts = metadata_dict['ldts'] -%}
-    {%- set rsrc = metadata_dict['rsrc'] -%}
+{%- set metadata_dict = fromyaml(yaml_metadata) -%}
+{%- set source_model = metadata_dict['source_model'] -%}
+{%- set ldts = metadata_dict['ldts'] -%}
+{%- set rsrc = metadata_dict['rsrc'] -%}
 {%- set hashed_columns = metadata_dict["hashed_columns"] -%}
 
 {{
-    datavault4dbt.stage(
-        source_model=source_model,
-        ldts=ldts,
-        rsrc=rsrc,
-        hashed_columns=hashed_columns,
-        include_source_columns = true
+datavault4dbt.stage(
+    source_model=source_model,
+    ldts=ldts,
+    rsrc=rsrc,
+    hashed_columns=hashed_columns
     )
 }}
