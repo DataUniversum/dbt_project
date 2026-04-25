875138390ad7084abcf649026febaddc18f315a2
Jaroslaw Syrokosz <jsyrokosz@datauniversum.com>
2026-04-25 11:14:14 +0000

    Fact to be  incremental by last 3 days

--- models/DataVault/Con/Fact_Customer.sql
+++ models/DataVault/Con/Fact_Customer.sql
@@ -1,4 +1,7 @@
-{{ config(materialized='view') }}
+{{ config(
+    materialized='incremental',
+    unique_key='date'
+) }}
 
 with customer_count as (
     select 
@@ -6,9 +9,12 @@ with customer_count as (
         ,count(*) as customer_count
         ,count(distinct hk_customer_h) as unique_customer_count
     from {{ ref('bdv_customer_pit') }}
+    {% if is_incremental() %}
+    where sdts >= current_date - interval '3 days'
+    {% endif %}
     group by all
 )
-select 
-    * 
+select * 
 from customer_count
 order by 1 desc
+
